db0e52f284dabc9f01fa898b3b5a5fea0d39115f
tonghaitao <490442001@qq.com>
2021-03-15 07:12:40 +0800

    c 语言中文网测试代码

--- header/Test.h
+++ header/Test.h
@@ -1,6 +1,13 @@
 #pragma once
 #include "Type.h"
 
+#define      TEMP_TEST                              0//临时借用
+
+#define      PTRDIFFWITHARRAY                       0//指针和数组不等测试
+#define      BINARY_OCTONARY_DECIMAL                0//二进制、八进制、十进制表示及输出
+#define      CHAR_DIFF_WITH_STRING                  0//字符和字符串的区别
+#define      IS_SUPPORT_NESTED_COMMENTS             0//检测是否支持嵌套注释
+#define      VAR_TYPE_FLOAT_DOUBLE_INT              1//变量类型:float、double存储方式
 extern u32 a[5];
 extern u32 *b;
 
